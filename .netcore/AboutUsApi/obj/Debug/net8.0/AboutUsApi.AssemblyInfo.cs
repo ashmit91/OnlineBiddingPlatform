e5923510d658cba1a0eb3138adbce7c0f8052da4
ashmit91 <107386093+ashmit91@users.noreply.github.com>
2025-08-05 12:55:16 +0530

    modify

--- .netcore/AboutUsApi/obj/Debug/net8.0/AboutUsApi.AssemblyInfo.cs
+++ .netcore/AboutUsApi/obj/Debug/net8.0/AboutUsApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AboutUsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d3c030292b1a06098419f10b96035ada3725c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AboutUsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AboutUsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
